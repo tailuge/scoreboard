2e58250088c5c163c36ee5c84521ca4d1fca95c2
tailuge <tailuge@gmail.com>
2026-03-04 07:51:51 +0000

    playwright

--- playwright.config.ts
+++ playwright.config.ts
@@ -3,7 +3,7 @@ import { defineConfig, devices } from "@playwright/test";
 export default defineConfig({
   testDir: "./src/playwright",
   fullyParallel: false,
-  reporter: "html",
+  reporter: [["html", { outputFolder: "public/playwright-report" }]],
   use: {
     baseURL: process.env.TEST_URL || "http://localhost:3000",
     trace: "on-first-retry",
