4a4a0f3d6395c52472cc748d9699972891ba7904
tailuge <tailuge@gmail.com>
2026-03-11 07:36:23 +0000

    tidyup

--- packages/messaging-client/src/index.ts
+++
@@ -1,5 +0,0 @@
-export * from "./messagingclient";
-export * from "./lobby";
-export * from "./table";
-export * from "./types";
-export * from "./nchanclient";
--- packages/messaging-client/src/utils/uid.ts
+++
@@ -1,3 +0,0 @@
-export function getUID() {
-  return "xxxxxxxx".replace(/x/g, () => Math.floor(Math.random() * 16).toString(16));
-}
