4c652229ed748d04aa29f1bd4e9803b260c6d8ae
tailuge <tailuge@gmail.com>
2026-03-27 07:13:07 +0000

    icons

--- src/utils/presence.ts
+++ src/utils/presence.ts
@@ -1,26 +1,36 @@
 export function getOriginEmoji(
   origin?: string,
-  ruleType?: string
+  ruleType?: string,
 ): { emoji: string; title: string } {
   if (origin?.includes("github")) {
-    return { emoji: "🔧", title: "github" }
+    return { emoji: "🔧", title: "github" };
   }
   if (origin?.includes("vercel")) {
-    return { emoji: "👥", title: "vercel" }
+    return { emoji: "👥", title: "vercel" };
   }
   if (origin?.includes("localhost")) {
-    return { emoji: "🏠", title: "localhost" }
+    return { emoji: "🏠", title: "localhost" };
+  }
+
+  if (ruleType === "spectator") {
+    return { emoji: "🔭", title: "spectator" };
+  }
+  if (ruleType === "replay") {
+    return { emoji: "👀", title: "replay" };
+  }
+  if (ruleType === "bot") {
+    return { emoji: "🤖", title: "bot" };
   }
 
   if (ruleType === "nineball") {
-    return { emoji: "⑨", title: "nineball" }
+    return { emoji: "⑨", title: "nineball" };
   }
   if (ruleType === "snooker") {
-    return { emoji: "⚪", title: "snooker" }
+    return { emoji: "⚪", title: "snooker" };
   }
   if (ruleType === "threecushion") {
-    return { emoji: "③", title: "threecushion" }
+    return { emoji: "③", title: "threecushion" };
   }
 
-  return { emoji: "🎮", title: "external" }
+  return { emoji: "🎮", title: "external" };
 }
